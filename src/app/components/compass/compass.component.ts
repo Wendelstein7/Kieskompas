400d00ccce9b40d10577f8afb3bdabc0f56848eb
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2023-11-07 20:55:03 +0100

    Axis labels now move

--- src/app/components/compass/compass.component.ts
+++ src/app/components/compass/compass.component.ts
@@ -12,4 +12,12 @@ export class CompassComponent {
     protected compassService: CompassService,
     protected partyInformationService: PartyInformationService,
   ) { }
+
+  protected clamp(input: number, min: number, max: number): number {
+    return input < min ? min : input > max ? max : input;
+  }
+
+  protected map(current: number, in_min: number, in_max: number, out_min: number, out_max: number): number {
+    return ((current - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  }
 }
